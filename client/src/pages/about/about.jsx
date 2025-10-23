e898e947839d1aa28e7ba29e60669965f5b6ba6d
gowthamvetri <gowthamvetriii@gmail.com>
2025-10-23 13:52:02 +0530

    About page initial

---
+++ client/src/pages/about/about.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import NavHeader from '@/components/common/navHeader'
+import AboutHeader from '@/components/about-view/aboutHeader'
+import OurStory from '@/components/about-view/ourStory'
+import WhyChoose from '@/components/about-view/whyChoose'
+import CareerAI from '@/components/about-view/careerAI'
+import AboutCEO from '@/components/about-view/aboutCEO'
+import OurMission from '@/components/about-view/ourMission'
+import OurVision from '@/components/about-view/ourVision'
+import ClientTestimonials from '@/components/about-view/clientTestimonials'
+
+
+const About = () => {
+  return (
+    <div className='w-full flex flex-col items-center justify-center py-3 gap-6'>
+      <div className="w-full">
+        <NavHeader/>
+      </div>
+      <AboutHeader/>
+      <OurStory/>
+      <WhyChoose/>
+      <CareerAI/>
+      <AboutCEO/>
+      <OurMission/>
+      <OurVision/>
+      <ClientTestimonials/>
+
+    </div>
+  )
+}
+
+export default About
